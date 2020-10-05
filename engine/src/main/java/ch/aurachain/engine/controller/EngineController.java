415f922bd8c33de094343f20cac7f3a58c41ba6f
Bogdan <gherla.bogdan@yahoo.ro>
2020-10-05 09:40:23 +0300

    add path engine

--- engine/src/main/java/ch/aurachain/engine/controller/EngineController.java
+++ engine/src/main/java/ch/aurachain/engine/controller/EngineController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api")
+@RequestMapping("engine/api")
 @RestController
 public class EngineController {
 
