1d983504baed1faeefcb28be3a660f42c1ba4d64
Bogdan <gherla.bogdan@yahoo.ro>
2020-10-05 22:50:31 +0300

    chane from cluster ip to service

--- engine/src/main/java/ch/aurachain/engine/client/WorkerClient.java
+++ engine/src/main/java/ch/aurachain/engine/client/WorkerClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "worker-cluster-ip-service")
+@FeignClient(name = "worker")
 public interface WorkerClient {
 
     @GetMapping("/api/value/{index}")
