25fa5235002fddb7ce4dcb31f139ab359aa3abdd
Bogdan <gherla.bogdan@yahoo.ro>
2020-10-05 12:34:49 +0300

    remove worker from path

--- worker/src/main/java/ch/aurachain/worker/controller/WorkerController.java
+++ worker/src/main/java/ch/aurachain/worker/controller/WorkerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/worker/api")
+@RequestMapping("/api")
 public class WorkerController {
 
     private final WorkerService workerService;
