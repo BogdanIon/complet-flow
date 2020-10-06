e9690b2713d6c31c2e0c3a1e36995caa24c8b5f1
Bogdan <gherla.bogdan@yahoo.ro>
2020-10-06 09:53:09 +0300

    edit worker port

--- engine/src/main/java/ch/aurachain/engine/client/WorkerClient.java
+++ engine/src/main/java/ch/aurachain/engine/client/WorkerClient.java
@@ -1,10 +1,12 @@
 package ch.aurachain.engine.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "worker-service")
+@Component
 public interface WorkerClient {
 
     @GetMapping("/api/value/{index}")
