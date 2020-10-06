c75b9df4b8cfdb44fe6028bae727006719b19c32
Bogdan <gherla.bogdan@yahoo.ro>
2020-10-06 09:15:10 +0300

    change from url to name in feign client

--- engine/src/main/java/ch/aurachain/engine/client/WorkerClient.java
+++ engine/src/main/java/ch/aurachain/engine/client/WorkerClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "worker-service")
+@FeignClient(name = "worker-service")
 public interface WorkerClient {
 
     @GetMapping("/api/value/{index}")
